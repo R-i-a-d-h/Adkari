bee8f8954d69522ea5dea4ae3a6b3fa173f8ce31
R-i-a-d-h <riadhaoumeur01@gmail.com>
2023-01-23 21:31:32 +0100

    feat: add a custom ui for starting page

---
+++ lib/starting_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class StartingPage extends StatelessWidget {
+  const StartingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Image(
+                image: AssetImage('images/img.png'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Text(
+                  'الَّذِينَ آمَنُواْ وَتَطْمَئِنُّ قُلُوبُهُم بِذِكْرِ اللَّهِ أَلاَ بِذِكْرِ اللَّهِ تَطْمَئِنُّ الْقُلُوبُ',
+                  style: TextStyle(
+                    fontFamily: 'Raleway',
+                    fontSize: 20,
+                    color: Color(0x25000000),
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
